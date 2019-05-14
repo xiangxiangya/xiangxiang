776cd9d2a5541c7fdd94052d2d132c7d9fe8d0e7
xiangxiangya <1119188979@qq.com>
2019-05-14 21:27:33 +0800

    c

--- 模拟实现strstr/模拟实现strstr/源.c
+++ 模拟实现strstr/模拟实现strstr/源.c
@@ -1,41 +1,65 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 #include<stdlib.h>
-int  My_strstr(char arr1[], char  arr2[])
+
+//const char *My_strstr(const char *str1, const char *str2)
+//{
+//	const char *s1 = NULL;
+//	const char *s2 = NULL;
+//	const char *start = str1;
+//	while (*start != '\0')
+//	{
+//     	s1 = start;//s1回退到上一次的下一个位置
+//		s2 = str2;//s2回退到str2的起始位置
+//		while (*s1 != '\0' && *s2 != '\0'&&*s1 == *s2)
+//		{
+//			s1++;
+//			s2++;
+//		}
+//		if (*s2 == '\0')
+//		{
+//			return start;
+//		}
+//		if (*s1 == '\0')
+//		{
+//			return NULL;
+//		}
+//		start++;
+//	}
+//	return NULL;
+//}
+const char *My_strstr(const char *str1, const char *str2)
+
 {
-	if (strlen(arr1) != strlen(arr2))
+	const char*s1 = NULL;
+	const char*s2 = NULL;
+	const char *flag = str1;
+	while (*str1 != '\0')
 	{
-		return -1;
-	}
-	char *p = arr1;
-	char *q = arr2;
-	while (*p != '\0')
-	{
-		if (*p != *q)
+		s1 = flag;//s1回退到上一次的下一个位置
+		s2 = str2;//s2回退到str2的起始位置
+		while (*s1 != '\0'&&*s2 != '\0'&&*s1 == *s2)
 		{
-			return -1;
+			s1++;
+			s2++;
 		}
-		else
+		if (*s2 == '\0')
 		{
-			p++;
-			q++;
+			return flag;
 		}
+		if (*s1 == '\0')
+		{
+			return NULL;
+		}
+		flag++;
 	}
-	return 1;
+	return NULL;
 }
 int main()
 {
-	char arr1[100] = "abcde";
-	char arr2[] = "abcde";
-	int result=My_strstr(arr1,arr2);
-	if (result == 1)
-	{
-		printf("找到了");
-	}
-	else
-	{
-		printf("没找到");
-	}
+	char str1[100] = "accdef";
+	char str2[] = "cd";
+	printf("%s\n", My_strstr(str1, str2));
 	system("pause");
 	return 0;
 }
