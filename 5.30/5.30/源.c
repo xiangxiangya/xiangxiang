b5c365be78e1270df5319f54881537e974930d30
xiangxiangya <1119188979@qq.com>
2019-05-31 13:01:28 +0800

    c

---
+++ 5.30/5.30/源.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+
+
+
+
+
+//typedef struct STUDENT
+//{
+//	char name[20];
+//
+//}student,*stu,**s;
+//int main()
+//{
+//	student a;
+//	stu c = { "abcd" };
+//	system("pause");
+//	return 0;
+//}
