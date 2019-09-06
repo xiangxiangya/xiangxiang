9b370951709276382c6c84b13638ede8494cafee
xiangxiangya <1119188979@qq.com>
2019-09-06 22:23:27 +0800

    c

--- 1/1/源.cpp
+++ 1/1/源.cpp
@@ -2,11 +2,9 @@
 #include<iostream>
 #include<stdlib.h>
 using namespace std;
-
-
 int main()
 {
-	cout << "hello world" << endl;
+	cout << "hello world!" << endl;
 	system("pause");
 	return 0;
 }
--- Project1/Project1/源.c
+++
@@ -1,5 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-int main()
-{
-	printf("hello word!");
-}
