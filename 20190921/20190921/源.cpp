824574eb31ffd7e58e6c6b981621b960c21ae001
xiangxiangya <1119188979@qq.com>
2019-10-16 11:01:55 +0800

    ，

--- 20190921/20190921/源.cpp
+++ 20190921/20190921/源.cpp
@@ -1,131 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdlib.h>
-#include<string>
+#include<vector>
 #include<iostream>
 using namespace std;
 
-#if 0
-//遍历输出的方式：
 int main()
 {
-	string s("hello world!");
-	for (auto e : s)//
+	vector<int> v(4, 100);
+	auto it = v.begin();
+	for (it; it != v.end(); it++)
 	{
-		cout << e << endl;
-	}
-
-	string::iterator it = s.begin();
-	for (it; it<s.end(); it++)
-	{
-		cout << *it << endl;
-	}
-
-	auto it = s.begin();
-	for (it; it != s.end(); it++)
-	{
-		cout << *it << endl;
-	}
-
-	system("pause");
-	return 0;
-}
-#endif
-
-//
-//class data
-//{
-//public:
-//	void fun(int a)
-//	{
-//
-//	}
-//private:
-//	int a;
-//};
-//
-//int main()
-//{
-//	data a;
-//	system("pause");
-//	return 0;
-//}
-
-#if 0
-int main()
-{
-	string s("hello world!");
-	int ret = s.find('?');
-	if (ret == string::npos)
-	{
-		cout << "no" << endl;
-	}
-	else
-	{
-		cout << ret << endl;
+		cout << *it;
 	}
 	system("pause");
 	return 0;
 }
-#endif
 
-#if 0
-int main()
-{
-	string s1;
-	string s2;
-	s1= "hello";
-	s2 = '!';
-	string s3;
-	s3 = s1 + s2;
-	system("pasue");
-	return 0;
-}
-#endif
-
-#if 0
-int main()
-{
-	string s(6, 's');
-	auto it = s.begin();
-	for (it; it != s.end(); it++)
-	{
-		cout << *it << endl;
-	}
-	system("pause");
-	return 0;
-}
-#endif
-
-
-#if 0
-int main()
-{
-	string s("hello");
-	s.reserve();
-	system("pause");
-	return 0;
-}
-#endif
-
-
-int _main()
-{
-	string s;
-	getline(cin, s);
-	cout << s;
-	system("pause");
-	return 0;
-}
-<<<<<<< HEAD
- 
-int main()
-{
-	cout << "hello" << endl;
-	return 0;
-}
-=======
-<<<<<<< HEAD
-//ok
-=======
->>>>>>> 0eeba4de1c6b0f3c2999a35cee1ca02c3326c8bb
->>>>>>> ea0f34cd8b3541be6f39bb45a947196ecb9039fc
